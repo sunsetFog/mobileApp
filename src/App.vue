75cd37d4611f57a0861af7a13b6344d41c9f8a8d
ivory <jsivory@itcom666.com>
2023-12-14 14:57:32 +0800

    2023-12-14 14:57

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-    <div class="watermark" v-if="watermark_list.indexOf($route.path) != -1"></div>
     <router-view />
     <PopupUnit></PopupUnit>
 </template>
@@ -11,12 +10,7 @@ export default {
     components: { PopupUnit },
     data() {
         return {
-            watermark_list: [
-                '/home',
-                '/priceManage/PriceAnalysis',
-                '/priceManage/index',
-                '/receivable/AR'
-            ]
+
         };
     },
     watch: {
@@ -35,15 +29,5 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-
-    .watermark {
-        position: fixed;
-        left: 0;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        background: #001018;
-        z-index: -1;
-    }
 }
 </style>
