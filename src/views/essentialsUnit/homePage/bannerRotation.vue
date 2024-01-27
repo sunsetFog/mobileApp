33a3fadbe0e44e9c49e9d417dab081b3165aafa1
ivory <jsivory@itcom666.com>
2024-01-27 18:46:34 +0800

    2024-1-27 18:46

--- src/views/essentialsUnit/homePage/bannerRotation.vue
+++ src/views/essentialsUnit/homePage/bannerRotation.vue
@@ -3,7 +3,7 @@
         <van-swipe indicator-color="white" style="width: 100%;height: 100%;">
             <van-swipe-item>
                 <div class="mei-you">
-                    <img src="@sky/static/mall/homePage/u40.png"/>
+                    <img src="@sky/rafaelDesign/static/mall/homePage/u40.png"/>
                 </div>
             </van-swipe-item>
             <van-swipe-item>2</van-swipe-item>
